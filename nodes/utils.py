f2ab702116b8d6f7ae19bd25d3d2b3d26027294d
Martin Rizzo <martinrizzo@gmail.com>
2024-11-11 17:49:41 -0300

    chore: update code formatting and minor comment corrections
    This commit makes minor adjustments to the code formatting and corrects
    some comments for better readability and consistency. No functional
    changes were introduced.

--- nodes/utils.py
+++ nodes/utils.py
@@ -184,11 +184,11 @@ def estimate_model_params(safetensors_path: os.PathLike, tensors_prefix: str = '
 #--------------------------------- LOGGER ----------------------------------#
 import logging
 
-class PixarFormatter(logging.Formatter):
+class CustomFormatter(logging.Formatter):
     """Custom formatter for the logger."""
-    RESET  = "\033[0m"    # Reset to default color
-    EMOJI  = "\U0001F3A8" # Emoji show before logger name
-    COLOR  = "\033[0;33m" # Yellow color for logger name color
+    EMOJI  = "\U0001F3A8" # the emoji show before logger name
+    COLOR  = "\033[0;33m" # yellow for the logger name color
+    RESET  = "\033[0m"    # reset to default color
     COLORS = {
         logging.INFO    : "\033[0;32m",  # GREEN
         logging.DEBUG   : "\033[0;34m",  # BLUE
@@ -198,6 +198,8 @@ class PixarFormatter(logging.Formatter):
     }
 
     def format(self, record):
+        """Override the default format method."""
+        # set color based on the log level and add an emoji before the logger name
         color = self.COLORS.get(record.levelno, self.RESET)
         record.name      = f"{self.EMOJI}{self.COLOR}{record.name}{self.RESET}"
         record.levelname = f"{color}{record.levelname}{self.RESET}"
@@ -205,10 +207,10 @@ class PixarFormatter(logging.Formatter):
 
 
 # Create a logger instance and set the custom formatter.
-logger = logging.getLogger("ComfyUI-x-PixArt")
+logger = logging.getLogger("x-PixArt")
 logger.propagate = False
 if not logger.handlers:
     handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(PixarFormatter("[%(name)s %(levelname)s] %(message)s"))
+    handler.setFormatter(CustomFormatter("[%(name)s %(levelname)s] %(message)s"))
     logger.addHandler(handler)
 logger.setLevel(logging.INFO)
