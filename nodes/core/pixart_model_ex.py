0baacd1d3ab08b0dfe2a288612c32963f0958e7f
Martin Rizzo <martinrizzo@gmail.com>
2025-01-03 13:07:33 -0300

    refactor(pixart_model_ex.py): improve state_dict format autodetection
    Refactored the code to enhance the robustness of the `state_dict`
    format autodetection mechanism. The updated logic now handles edge
    cases more effectively and provides clearer documentation through
    improved comments. This makes the code easier to follow and understand.

--- nodes/core/pixart_model_ex.py
+++ nodes/core/pixart_model_ex.py
@@ -1,7 +1,7 @@
 """
 File    : pixart_model_ex.py
-Purpose : Extension of the PixArtModel class to include additional functionality.
-          Includes autodetection of model comfiguation and HF diffusers format support.
+Purpose : Extension of the PixArtModel class to include additional useful funtionality.
+          (includes autodetection of model comfiguation and support for HF diffusers format)
 Author  : Martin Rizzo | <martinrizzo@gmail.com>
 Date    : Dec 15, 2024
 Repo    : https://github.com/martin-rizzo/ComfyUI-xPixArt
@@ -15,20 +15,6 @@ import torch
 from .pixart_model import PixArtModel
 
 
-def _detect_prefix(state_dict: dict, key_1:str, key_2:str) -> str:
-    """Detects the model prefix based on the presence of two keys."""
-    for key in state_dict.keys():
-        if key.endswith(key_1):
-            prefix = key[:-len(key_1)]
-            if f"{prefix}{key_2}" in state_dict:
-                return prefix
-        elif key.endswith(key_2):
-            prefix = key[:-len(key_2)]
-            if f"{prefix}{key_1}" in state_dict:
-                return prefix
-    return ""
-
-
 def _find_tensor(state_dict: dict, template_key: str, subkey: str = None) -> torch.Tensor:
     """Finds a tensor in the state_dict using a template key."""
     if subkey is None:
@@ -37,80 +23,60 @@ def _find_tensor(state_dict: dict, template_key: str, subkey: str = None) -> tor
         parts = template_key.split("|")
         return state_dict.get(parts[0] + subkey + parts[2])
 
-def _get_number(key: str, prefix: str):
+
+def _get_number(key: str, prefix: str) -> int:
     """Extracts a number from a key based on a given prefix. Returns 0 if not found."""
     number_str = key[len(prefix):].split('.',1)[0]
     return int(number_str) if number_str.isdigit() else 0
+
+
+def _normalize_prefix(prefix: str) -> str:
+    """Normalize a given prefix by ensuring it ends with a dot."""
+    prefix = prefix.strip()
+    if prefix and prefix != "*" and not prefix.endswith('.'):
+        prefix += '.'
+    return prefix
 
 
 #------------------- SUPPORT FOR DIFFERENT MODEL FORMATS -------------------#
 
 class _PixArtFormat:
 
-    def __call__(self, state_dict: dict, prefix: str) -> tuple[dict, str]:
-        KEY_1 = "t_embedder.mlp.0.weight"
-        KEY_2 = "final_layer.scale_shift_table"
-        original_state_dict, original_prefix = state_dict, prefix
+    # format must have its `SIGNATURE_TENSORS` constant with
+    # the names of tensors that are characteristic to it
+    SIGNATURE_TENSORS = [
+        "t_embedder.mlp.0.weight",
+        "final_layer.scale_shift_table"
+    ]
 
-        # auto-detect prefix if the special prefix "*" is used
-        if prefix == "*":
-            prefix = _detect_prefix(state_dict, KEY_1, KEY_2)
-
-        # ensure that prefix always ends with a period '.'
-        if prefix and not prefix.endswith('.'):
-            prefix += '.'
-
-        # if the characteristic tensors of this format are not present
-        # then it is not a native PixArt model format, so we return the original values
-        if (f"{prefix}{KEY_1}" not in state_dict) or (f"{prefix}{KEY_2}" not in state_dict):
-            return original_state_dict, original_prefix
-
-        # remove prefix
-        if prefix:
-            state_dict = {key[len(prefix):]: tensor for key, tensor in state_dict.items() if key.startswith(prefix)}
-            prefix     = ""
-
-        return state_dict, prefix
+    def normalize_state_dict(self, state_dict: dict) -> dict:
+        return state_dict
 
 
 class _DiffusersFormat:
-    """
-    Converts a state dictionary from Hugging Face Diffusers format to standard PixArt format.
 
-    This converter handles specific mappings between Hugging Face Diffusers model
-    parameters and standard PixArt parameters. It also handles specific cases where
-    certain parameters need to be renamed or modified.
+    # format must have its `SIGNATURE_TENSORS` constant with
+    # the names of tensors that are characteristic to it
+    SIGNATURE_TENSORS = [
+        "adaln_single.emb.timestep_embedder.linear_2.bias",
+        "scale_shift_table"
+    ]
 
-    Args:
-        state_dict (dict): The state dictionary from Hugging Face Diffusers format.
-        prefix     (str) : A prefix to filter keys in the state dictionary.
-                           If "*", it will automatically detect the correct prefix.
-    Returns:
-        A tuple containing the converted state dictionary and the updated prefix.
-    """
-    def __call__(self, state_dict: dict, prefix: str) -> tuple[dict, str]:
-        KEY_1 = "adaln_single.emb.timestep_embedder.linear_2.bias"
-        KEY_2 = "scale_shift_table"
+    def normalize_state_dict(self, state_dict: dict) -> dict:
+        """
+        Normalizes the state_dict from HF Diffusers format to PixArt native format.
+
+        This converter handles specific mappings between Hugging Face Diffusers model
+        parameters and standard PixArt parameters. It also handles specific cases where
+        certain parameters need to be renamed, modified or merged.
+
+        Args:
+            state_dict (dict): The state dictionary from Hugging Face Diffusers format.
+        Returns:
+            The normalized state dictionary in PixArt native format.
+        """
         KV_TAG, QKV_TAG  = self.KV_TAG, self.QKV_TAG
-        original_state_dict, original_prefix = state_dict, prefix
 
-        # auto-detect prefix if the special prefix "*" is used
-        if prefix == "*":
-            prefix = _detect_prefix(state_dict, KEY_1, KEY_2)
-
-        # ensure that prefix always ends with a period '.'
-        if prefix and not prefix.endswith('.'):
-            prefix += '.'
-
-        # if the characteristic tensors of this format are not present
-        # then it is not HF diffusers PixArt model, so we return the original values
-        if (f"{prefix}{KEY_1}" not in state_dict) or (f"{prefix}{KEY_2}" not in state_dict):
-            return original_state_dict, original_prefix
-
-        # remove prefix and generate a pixart compatible state dict
-        if prefix:
-            state_dict = {key[len(prefix):]: tensor for key, tensor in state_dict.items() if key.startswith(prefix)}
-            prefix     = ""
         pixart_state_dict = {}
         for pixart_key, diffusers_key in self.get_conversion_table():
 
@@ -132,7 +98,8 @@ class _DiffusersFormat:
                 if (_tensor_q is not None ) and (_tensor_k is not None) and (_tensor_v is not None):
                     pixart_state_dict[pixart_key] = torch.cat((_tensor_q, _tensor_k, _tensor_v))
 
-        return pixart_state_dict, prefix
+        return pixart_state_dict
+
 
     # tags used to map tensor names in the conversion table template
     DEPTH_TAG = "{{depth}}"
@@ -206,17 +173,15 @@ class _DiffusersFormat:
 
 
 # The list of supported formats for conversion.
-# Each element is a callable that takes a `state_dict` and `prefix` as input
-# and returns a new state_dict with the parameters in the native pixart format.
+# Each element has a `normalize_state_dict()` function that takes a `state_dict`
+# as input and returns a new `state_dict` with keys and tensors in native PixArt format.
 _SUPPORTED_FORMATS = [_PixArtFormat(), _DiffusersFormat()]
+
 
 
 #===========================================================================#
 #///////////////////////////// PIXART MODEL EX /////////////////////////////#
 #===========================================================================#
-
-
-
 class PixArtModelEx(PixArtModel):
 
     @classmethod
@@ -241,6 +206,14 @@ class PixArtModelEx(PixArtModel):
                                a prefix, and returns a tuple containing the modified state
                                dictionary and the detected prefix. Default is _SUPPORTED_FORMATS.
         """
+
+        # always ensure that `prefix` is normalized
+        prefix = _normalize_prefix(prefix)
+
+        # auto-detect prefix in the special case when prefix is set to "*"
+        if prefix == "*":
+            prefix = cls.detect_prefix(state_dict, default="")
+
         # normalize the state dictionary using the provided format converters
         state_dict, prefix = cls.normalize_state_dict(state_dict, prefix, supported_formats)
 
@@ -253,9 +226,120 @@ class PixArtModelEx(PixArtModel):
         return model
 
 
-    @classmethod
-    def infer_model_config(cls,
-                           state_dict: dict,
+    @property
+    def dtype(self):
+        """Returns the data type of the model parameters."""
+        return self.x_embedder.proj.weight.dtype
+
+
+    @property
+    def device(self):
+        """Returns the device on which the model parameters are located."""
+        return self.x_embedder.proj.weight.device
+
+
+    def freeze(self) -> None:
+        """Freeze all parameters of the model to prevent them from being updated during inference."""
+        for param in self.parameters():
+            param.requires_grad = False
+        self.eval()
+
+
+    def unfreeze(self) -> None:
+        """Unfreeze all parameters of the model to allow them to be updated during training."""
+        for param in self.parameters():
+            param.requires_grad = True
+        self.train()
+
+
+    #====== Useful Static Methods ======#
+
+    @staticmethod
+    def detect_prefix(state_dict       : dict,
+                      prefix           : str  = "*",
+                      supported_formats: list = _SUPPORTED_FORMATS
+                      ) -> str:
+        """
+        Detects the prefix used by a given state dictionary.
+
+        Args:
+          state_dict       : A dictionary containing the model's state to detect.
+          prefix           : An optional tentative prefix where detecting will be performed.
+          supported_formats: An optional list of supported formats to try when detecting.
+                             (this parameter normally does not need to be provided)
+        Returns:
+          The detected prefix used by the given state dictionary.
+          (if no prefix is detected, it returns `None`)
+
+        """
+        prefix = _normalize_prefix(prefix)
+        for format in supported_formats:
+            name1 = format.SIGNATURE_TENSORS[0]
+            name2 = format.SIGNATURE_TENSORS[1]
+
+            # the default auto-detection mechanism
+            if prefix == "*":
+                for key in state_dict.keys():
+                    if key.endswith(name1):                     # check for the first signature tensor
+                        detected_prefix = key[:-len(name1)]
+                        if detected_prefix+name2 in state_dict: # check for the second signature tensor
+                            return detected_prefix
+
+            # if a tentative prefix was provided, use it for detection
+            elif prefix+name1 in state_dict and prefix+name2 in state_dict:
+                    return prefix
+
+        # if no prefix is found, return `None`
+        return None
+
+
+    @staticmethod
+    def normalize_state_dict(state_dict       : dict,
+                             prefix           : str  = "",
+                             supported_formats: list = _SUPPORTED_FORMATS
+                             ) -> tuple[dict, str]:
+        """
+        Normalizes a state dictionary to match the native PixArt format.
+
+        Args:
+           state_dict       : A dictionary containing the PixArt model's state.
+           prefix           : An optional prefix used to filter the state dictionary keys.
+           supported_formats: An optional list of supported formats to try when normalizing.
+                              (this parameter normally does not need to be provided)
+        Returns:
+           A tuple containing:
+           - The normalized dictionary containing the PixArt model's state.
+           - The new prefix to use with the normalized dictionary.
+
+        """
+        assert prefix != "*", \
+            f"PixArtModelEx: Prefix cannot be '*' when normalizing model tensors. Please provide a valid prefix."
+
+        # always ensure that `prefix` is normalized
+        prefix = _normalize_prefix(prefix)
+
+        # remove the prefix from `state_dict`
+        if prefix:
+            state_dict = {key[len(prefix):]: tensor for key, tensor in state_dict.items() if key.startswith(prefix)}
+            prefix     = ""
+
+        # iterate over all supported formats to find the appropriate one that can normalize the state_dict
+        # (supported formats are usually `_PixArtFormat` and `_DiffusersFormat`)
+        for format in supported_formats:
+
+            # check whether this is the correct format to normalize `state_dict`
+            if f"{prefix}{format.SIGNATURE_TENSORS[0]}" not in state_dict or \
+               f"{prefix}{format.SIGNATURE_TENSORS[1]}" not in state_dict:
+                continue
+
+            # normalize `state_dict` using this supported format
+            state_dict = format.normalize_state_dict(state_dict)
+
+        return state_dict, prefix
+
+
+    @staticmethod
+    def infer_model_config(state_dict: dict,
                            prefix    : str = "",
                            resolution: int = 1024
                            ) -> dict:
@@ -266,6 +350,9 @@ class PixArtModelEx(PixArtModel):
            state_dict: A dictionary containing the PixArt model's state.
            prefix    : An optional prefix used to filter the state dictionary keys.
            resolution: The base resolution the model is intended to work at.
+        Returns:
+           A dictionary containing the model's configuration.
+
         """
         assert prefix != "*", \
             f"PixArtModelEx: Prefix cannot be '*' when inferring model config. Please provide a valid prefix."
@@ -278,9 +365,8 @@ class PixArtModelEx(PixArtModel):
         DEFAULT_IN_CHANNELS  =    4
         DEFAULT_PATCH_SIZE   =    2
 
-        # normalize the prefix to ensure it ends with a dot
-        if prefix and not prefix.endswith("."):
-            prefix += "."
+        # always ensure that `prefix` is normalized
+        prefix = _normalize_prefix(prefix)
 
         # determine the number of layers in the model
         # this is done by finding the block with the highest number
@@ -321,39 +407,3 @@ class PixArtModelEx(PixArtModel):
         }
         return config
 
-
-    @property
-    def dtype(self):
-        """Returns the data type of the model parameters."""
-        return self.x_embedder.proj.weight.dtype
-
-
-    @property
-    def device(self):
-        """Returns the device on which the model parameters are located."""
-        return self.x_embedder.proj.weight.device
-
-
-    @staticmethod
-    def normalize_state_dict(state_dict       : dict,
-                             prefix           : str  = "",
-                             supported_formats: list = _SUPPORTED_FORMATS
-                             ) -> tuple[dict, str]:
-        """Normalizes a state dictionary to match the native PixArt format."""
-        for supported_format in supported_formats:
-            state_dict, prefix = supported_format(state_dict, prefix)
-        return state_dict, prefix
-
-
-    def freeze(self) -> None:
-        """Freeze all parameters of the model to prevent them from being updated during inference."""
-        for param in self.parameters():
-            param.requires_grad = False
-        self.eval()
-
-
-    def unfreeze(self) -> None:
-        """Unfreeze all parameters of the model to allow them to be updated during training."""
-        for param in self.parameters():
-            param.requires_grad = True
-        self.train()
