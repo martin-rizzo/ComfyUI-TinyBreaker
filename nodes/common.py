83e56f56b2efc423ed8aecfaea1206ad04396ad2
Martin Rizzo <martinrizzo@gmail.com>
2025-01-18 13:00:01 -0300

    fix: Normalize options to lowercase for SetImageCFGAndSeed node
    Fixed an issue where options displayed by the "SetImageCFGAndSeed"
    were not lowercase, ensuring consistency with native ComfyUI nodes.

--- nodes/common.py
+++ nodes/common.py
@@ -90,29 +90,32 @@ def expand_variables(template  : str,
 #-------------------------- IMAGE SCALES & RATIOS --------------------------#
 
 LANDSCAPE_SIZES_BY_ASPECT_RATIO = {
-    "1:1 square"      : (1024.0, 1024.0),
-    "4:3 tv"          : (1182.4,  886.8),
-    "48:35 (35 mm)"   : (1199.2,  874.4),
-    "71:50 ~IMAX"     : (1220.2,  859.3),
-    "3:2 photo"       : (1254.1,  836.1),
-    "16:10 wide"      : (1295.3,  809.5),
-    "16:9 hdtv"       : (1365.3,  768.0),
-    "2:1 mobile"      : (1448.2,  724.0),
-    "21:9 ultrawide"  : (1564.2,  670.4),
-    "12:5 anamorphic" : (1586.4,  661.0),
-    "70:27 cinerama"  : (1648.8,  636.0),
-    "32:9 s.ultrawide": (1930.9,  543.0),
+    "1:1  (square)"      : (1024.0, 1024.0),
+    "4:3  (tv)"          : (1182.4,  886.8),
+    "48:35  (35 mm)"     : (1199.2,  874.4),
+    "71:50  (~imax)"     : (1220.2,  859.3),
+    "3:2  (photo)"       : (1254.1,  836.1),
+    "16:10  (wide)"      : (1295.3,  809.5),
+    "16:9  (hdtv)"       : (1365.3,  768.0),
+    "2:1  (mobile)"      : (1448.2,  724.0),
+    "21:9  (ultrawide)"  : (1564.2,  670.4),
+    "12:5  (anamorphic)" : (1586.4,  661.0),
+    "70:27  (cinerama)"  : (1648.8,  636.0),
+    "32:9  (s.ultrawide)": (1930.9,  543.0),
 }
 SCALES_BY_NAME = {
-    "Small"  : 0.82,
-    "Medium" : 1.0,
-    "Large"  : 1.22,
+    "small"  : 0.82,
+    "medium" : 1.0,
+    "large"  : 1.22,
 }
-ORIENTATIONS = ["Landscape", "Portrait"]
+ORIENTATIONS = [
+    "landscape",
+    "portrait"
+]
 
 DEFAULT_ASPECT_RATIO = "1:1 square"
-DEFAULT_SCALE_NAME   = "Large"
-DEFAULT_ORIENTATION  = "Landscape"
+DEFAULT_SCALE_NAME   = "large"
+DEFAULT_ORIENTATION  = "landscape"
 
 
 def normalize_aspect_ratio(aspect_ratio: str, *, orientation: str = DEFAULT_ORIENTATION) -> str:
