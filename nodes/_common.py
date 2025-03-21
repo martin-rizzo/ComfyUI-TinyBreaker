2f2f786dd7149e32e6381e2307b1fa2ff9d67a5c
Martin Rizzo <martinrizzo@gmail.com>
2025-03-21 12:08:37 -0300

    refactor: Expose calculate_sigmas() function for wider usage
    This commit refactors the `calculate_sigmas()` function, making it
    publicly accessible from _common.py. Previously an internal method
    of the DenoisingParams class with arguments tailored for its specific
    use case, the function is now generalized and readily usable
    throughout the project.

--- nodes/_common.py
+++ nodes/_common.py
@@ -12,33 +12,13 @@ License : MIT
 _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 """
 import re
+import torch
+from comfy.samplers  import calculate_sigmas as comfy_calculate_sigmas, \
+                            KSampler
 from .core.genparams import GenParams
 
 
-#----------------------- STRING MANIPULATION --------------------------#
-
-def ireplace(text: str, old: str, new: str, count: int = -1) -> str:
-    """
-    Replaces all occurrences of `old` in `text` with `new`, case-insensitive.
-    If count is given, only the first `count` occurrences are replaced.
-    """
-    lower_text , lower_old = text.lower(), old.lower()
-    index_start, index_end = 0, lower_text.find(lower_old, 0)
-    if index_end == -1 or len(lower_text) != len(text):
-        return text
-
-    output = ""
-    lower_old_length = len(lower_old)
-    while index_end != -1 and count != 0:
-        output += text[index_start:index_end] + new
-        index_start = index_end + lower_old_length
-        index_end   = lower_text.find(lower_old, index_start)
-        if count > 0:
-            count -= 1
-    return output + text[index_start:]
-
-
-#-------------------------- IMAGE SCALES & RATIOS --------------------------#
+#----------------------- IMAGE SCALE/RATIO CONSTANTS -----------------------#
 
 LANDSCAPE_SIZES_BY_ASPECT_RATIO = {
     "1:1  (square)"      : (1024.0, 1024.0),
@@ -79,6 +59,8 @@ DEFAULT_ORIENTATION  = "landscape"
 DEFAULT_DETAIL_LEVEL = "normal"
 
 
+#------------------------------ ASPECT RATIO -------------------------------#
+
 def normalize_aspect_ratio(aspect_ratio: str, *, force_orientation: str = None) -> str:
     """
     Normalize aspect ratio to a string of the form 'width:height'.
@@ -115,6 +97,110 @@ def normalize_aspect_ratio(aspect_ratio: str, *, force_orientation: str = None) 
             width, height = height, width
 
     return f"{width}:{height}"
+
+
+#--------------------------------- SIGMAS ----------------------------------#
+
+# Sampler names that will automatically discard the penultimate sigma value:
+#  - "dpm_2", "dpm_2_ancestral", "uni_pc", "uni_pc_bh2"
+DISCARD_PENULTIMATE_SIGMA_SAMPLERS = KSampler.DISCARD_PENULTIMATE_SIGMA_SAMPLERS
+
+def calculate_sigmas(model_sampling           : object,
+                     sampler                  : str,
+                     scheduler                : str,
+                     steps                    : int,
+                     steps_start              : int = 0,
+                     steps_end                : int = 10000,
+                     steps_nfactor            : int = 0,
+                     discard_penultimate_sigma: bool | None = None,
+                     ) -> torch.Tensor:
+    """
+    Calculates the sigma values for a given model, sampler and scheduler.
+    Args:
+        model_sampling   (object): The sampling object for the model.
+        sampler            (str) : The name of the sampler to use (e.g., "euler", "heun")
+        scheduler          (str) : The name of the scheduler to use (e.g., "karras")
+        steps              (int) : The number of steps to calculate sigma values for.
+        steps_start    (optional): The starting step for sigma calculation.
+        steps_end      (optional): The ending step for sigma calculation.
+        steps_nfactor  (optional): A custom value to adjust the number of denoising steps;
+                                   positive values will increase the number of denoising steps,
+                                   negative values will decrease it.
+        discard_penultimate_sigma (optional): Whether to discard the penultimate sigma value.
+                                              If not provided, it will be inferred from the sampler name.
+    Returns:
+        A tensor containing the sigma values for each step.
+    """
+    steps       = min(steps, steps_end)
+    steps_start = max(0, steps_start)
+
+    # if discard_penultimate_sigma is not explicitly provided,
+    # infer it from the sampler name
+    if discard_penultimate_sigma is None:
+        discard_penultimate_sigma = sampler in DISCARD_PENULTIMATE_SIGMA_SAMPLERS
+
+    # 'steps_nfactor' modulates the number of steps. A positive value signifies
+    # a proportional increase, while a negative value indicates a proportional
+    # decrease in the total steps.
+    # Note: The specific effect is context-dependent and may require careful adjustment.
+    if steps_nfactor > 0:
+        nstart = min( steps_nfactor, steps_start )
+        nend   = steps_nfactor - nstart
+    elif steps_nfactor < 0:
+        nstart = steps_nfactor
+        nend   = 0
+    else:
+        nstart, nend = 0, 0
+    steps_start -= nstart
+    steps_end   += nend
+    steps       += nend
+
+    # if no steps are specified, return an empty tensor
+    # this is useful when no sampling should be performed
+    if steps <= 0 or steps_start >= steps:
+        return torch.FloatTensor([])
+
+    # if discard was requested,
+    # first calculate all sigmas (plus one) and then discard the penultimate one
+    if discard_penultimate_sigma:
+        sigmas = comfy_calculate_sigmas(model_sampling, scheduler, steps+1).cpu()
+        sigmas = torch.cat((sigmas[:-2], sigmas[-1:]))
+    else:
+        sigmas = comfy_calculate_sigmas(model_sampling, scheduler, steps).cpu()
+
+    # the original code from ComfyUI uses this adjustment in some parts,
+    # no idea if it's necessary, but I'll leave it just in case
+    sigmas = sigmas[-( steps+1 ):]
+
+    # discard all sigmas before the specified start step
+    steps_start = min(steps_start, len(sigmas))
+    if steps_start > 0:
+        sigmas = sigmas[steps_start:]
+
+    return sigmas
+
+
+#--------------------------- STRING MANIPULATION ---------------------------#
+
+def ireplace(text: str, old: str, new: str, count: int = -1) -> str:
+    """
+    Replaces all occurrences of `old` in `text` with `new`, case-insensitive.
+    If count is given, only the first `count` occurrences are replaced.
+    """
+    lower_text , lower_old = text.lower(), old.lower()
+    index_start, index_end = 0, lower_text.find(lower_old, 0)
+    if index_end == -1 or len(lower_text) != len(text):
+        return text
+
+    output = ""
+    lower_old_length = len(lower_old)
+    while index_end != -1 and count != 0:
+        output += text[index_start:index_end] + new
+        index_start = index_end + lower_old_length
+        index_end   = lower_text.find(lower_old, index_start)
+        if count > 0:
+            count -= 1
+    return output + text[index_start:]
 
 
 #--------------------------- GENPARAMS FROM ARGS ---------------------------#
--- nodes/_denoising_params.py
+++ nodes/_denoising_params.py
@@ -13,9 +13,9 @@ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
 """
 import torch
 import comfy.samplers
+from ._common        import calculate_sigmas
 from .core.genparams import GenParams, normalize_prefix
 from .utils.system   import logger
-_DISCARD_PENULTIMATE_SIGMA_SAMPLERS = comfy.samplers.KSampler.DISCARD_PENULTIMATE_SIGMA_SAMPLERS
 
 
 class DenoisingParams:
@@ -72,15 +72,18 @@ class DenoisingParams:
         steps_nfactor = int(steps_nfactor) if steps_nfactor is not None else 0
         cfg           = float(cfg)         if cfg           is not None else 3.5
         noise_seed    = int(noise_seed)    if noise_seed    is not None else 1
-        if discard_penultimate_sigma is not None:
-            discard_penultimate_sigma = bool(discard_penultimate_sigma)
-        else:
-            discard_penultimate_sigma = sampler in _DISCARD_PENULTIMATE_SIGMA_SAMPLERS
 
         # if sigmas is not provided, calculate it from the model
         if not isinstance(sigmas, torch.Tensor):
             if model:
-                sigmas = self._calculate_sigmas(model, scheduler, steps, steps_start, steps_end, steps_nfactor, discard_penultimate_sigma)
+                sigmas = calculate_sigmas(model.get_model_object("model_sampling"),
+                                          sampler,
+                                          scheduler,
+                                          steps,
+                                          steps_start,
+                                          steps_end,
+                                          steps_nfactor,
+                                          discard_penultimate_sigma)
             else:
                 logger.warning("sigmas could not be calculated because the model was not provided")
                 sigmas = torch.tensor([1.0])
@@ -126,61 +129,6 @@ class DenoisingParams:
                    cfg           = genparams.get( f"{prefix}cfg"           ),
                    noise_seed    = genparams.get( f"{prefix}noise_seed"    ),
                    model         = model_to_sample)
-
-
-    @staticmethod
-    def _calculate_sigmas(model                    : object,
-                          scheduler                : str,
-                          steps                    : int,
-                          steps_start              : int  = 0,
-                          steps_end                : int  = 10000,
-                          steps_nfactor            : int  = 0,
-                          discard_penultimate_sigma: bool = False,
-                          ) -> torch.Tensor:
-        """Calculates the sigma values for a given model and scheduler."""
-
-        steps       = min(steps, steps_end)
-        steps_start = max(0, steps_start)
-
-        # 'steps_nfactor' modulates the number of steps. A positive value signifies
-        # a proportional increase, while a negative value indicates a proportional
-        # decrease in the total steps.
-        # Note: The specific effect is context-dependent and may require careful adjustment.
-        if steps_nfactor > 0:
-            nstart = min( steps_nfactor, steps_start )
-            nend   = steps_nfactor - nstart
-        elif steps_nfactor < 0:
-            nstart = steps_nfactor
-            nend   = 0
-        else:
-            nstart, nend = 0, 0
-        steps_start -= nstart
-        steps_end   += nend
-        steps       += nend
-
-        # if no steps are specified, return an empty tensor
-        # this is useful when no sampling should be performed
-        if steps <= 0 or steps_start >= steps:
-            return torch.FloatTensor([])
-
-        # if discard was requested,
-        # first calculate all sigmas (plus one) and then discard the penultimate one
-        if discard_penultimate_sigma:
-            sigmas = comfy.samplers.calculate_sigmas(model.get_model_object("model_sampling"), scheduler, steps+1).cpu()
-            sigmas = torch.cat((sigmas[:-2], sigmas[-1:]))
-        else:
-            sigmas = comfy.samplers.calculate_sigmas(model.get_model_object("model_sampling"), scheduler, steps).cpu()
-
-        # the original code from ComfyUI uses this adjustment in some parts,
-        # no idea if it's necessary, but I'll leave it just in case
-        sigmas = sigmas[-( steps+1 ):]
-
-        # discard all sigmas before the specified start step
-        steps_start = min(steps_start, len(sigmas))
-        if steps_start > 0:
-            sigmas = sigmas[steps_start:]
-
-        return sigmas
 
 
     def __str__(self):
